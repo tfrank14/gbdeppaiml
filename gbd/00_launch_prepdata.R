899ee56625a6f8ac77bac14a3db5769a904656cd
djahag <djahag@uw.edu>
2018-12-27 07:51:22 -0800

    launch script for main_prep_data

--- gbd/00_launch_prepdata.R
+++ gbd/00_launch_prepdata.R
@@ -3,7 +3,7 @@ rm(list=ls())
 windows <- Sys.info()[1][["sysname"]]=="Windows"
 root <- ifelse(windows,"J:/","/home/j/")
 user <- ifelse(windows, Sys.getenv("USERNAME"), Sys.getenv("USER"))
-code.dir <- paste0(ifelse(windows, "H:", paste0("/homes/", user)), "/gbdeppaiml/")
+code.dir <- paste0(ifelse(windows, "H:", paste0("/homes/", user)), "/eppasm-1/")
 date <- substr(gsub("-","",Sys.Date()),3,8)
 
 ## Packages
@@ -15,6 +15,7 @@ cluster.project <- "proj_hiv"
 ### Functions
 library(mortdb, lib = "/home/j/WORK/02_mortality/shared/r")
 
+
 ### Tables
 loc.table <- data.table(get_locations(hiv_metadata = T))
 
@@ -22,16 +23,16 @@ loc.table <- data.table(get_locations(hiv_metadata = T))
 epp.list <- sort(loc.table[epp == 1, ihme_loc_id])
 loc.list <- epp.list
 
-loc ="MWI"
+loc.list <- epp.list[-grep("IND", epp.list)]
+
 ## Launch prepare locations file
 for(loc in loc.list) {
-    prep.files.string <- paste0("qsub -P ", cluster.project, " -pe multi_slot 1 ", 
+    prep.files.string <- paste0("qsub -pe multi_slot 1 -P ", cluster.project, " ", 
                          "-e /share/temp/sgeoutput/", user, "/errors ",
                          "-o /share/temp/sgeoutput/", user, "/output ",
                          "-N ", loc, "_prep_data ",
                          "/homes/", user, "/gbdeppaiml/gbd/singR_shell.sh ", 
-                         code.dir, "R/prep_pjnz_data.R ",
-                         loc)
+                         "/homes/djahag/gbdeppaiml/gbd/main_prep_data.R ", loc)
     print(prep.files.string)
     system(prep.files.string)
         
