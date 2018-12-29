615735bbea51b13417855d3b23215d7c0f80c5bb
djahag <djahag@uw.edu>
2018-12-29 14:35:05 -0800

    Added ANC backcast

--- R/read_spec_object.R
+++ R/read_spec_object.R
@@ -33,16 +33,23 @@ read_spec_object <- function(loc, i, start.year = 1970, stop.year = 2019, trans.
   }
   
   ## ANC data
-  # high.risk.list <- loc.table[epp == 1 & collapse_subpop == 0 & !grepl("ZAF", ihme_loc_id) & !grepl("KEN", ihme_loc_id), ihme_loc_id]
-  # ken.anc.path <- paste0(root, "WORK/04_epi/01_database/02_data/hiv/data/prepped/kenya_anc_map.csv")
-  # ken.anc <- fread(ken.anc.path)
-  # no.anc.ken <- setdiff(loc.table[epp == 1 & grepl("KEN", ihme_loc_id), ihme_loc_id], ken.anc$ihme_loc_id)
-  # if(loc %in% c(high.risk.list, "PNG", no.anc.ken)) {
-  # 	anc.backcast <- F
-  # }
-  # if(anc.backcast) {
-  # 	dt <- sub.anc(loc, dt)
-  # }
+  if(anc.sub){
+  high.risk.list <- loc.table[epp == 1 & collapse_subpop == 0 & !grepl("ZAF", ihme_loc_id) & !grepl("KEN", ihme_loc_id), ihme_loc_id]
+  ken.anc.path <- paste0(root, "WORK/04_epi/01_database/02_data/hiv/data/prepped/kenya_anc_map.csv")
+  ken.anc <- fread(ken.anc.path)
+  no.anc.ken <- setdiff(loc.table[epp == 1 & grepl("KEN", ihme_loc_id), ihme_loc_id], ken.anc$ihme_loc_id)
+  if(loc %in% c(high.risk.list, "PNG", no.anc.ken)) {
+    anc.backcast <- F
+  }
+  
+  if(anc.backcast) {
+    print("Substituting ANC backcast output")
+    dt <- sub.anc(loc, dt)
+  }
+  
+  return(dt)
+
+} 
   
   ## Transition parameters
   if(trans.params.sub) {
