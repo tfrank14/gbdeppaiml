f9292f45ab5fabe72709a3e30cd7e728d26ef3fe
djahag <djahag@uw.edu>
2019-01-11 09:29:18 -0800

    revert the directory

--- gbd/gbd_prep_inputs.R
+++ gbd/gbd_prep_inputs.R
@@ -16,8 +16,8 @@ if(length(args) > 0) {
 	proj.end <- 2019
 }
 
-#out.dir <- paste0('/ihme/hiv/epp_input/gbd19/', run.name, "/")
-out.dir <- paste0('/share/homes/djahag/eppasm-1-trials/', run.name, "/")
+out.dir <- paste0('/ihme/hiv/epp_input/gbd19/', run.name, "/")
+#out.dir <- paste0('/share/homes/djahag/eppasm-1-trials/', run.name, "/")
 dir.create(out.dir, showWarnings = FALSE)
 
 ## Functions
@@ -38,6 +38,16 @@ invisible(lapply(epp.locs, function(c.location_id) {
   out.pop <- copy(pop[location_id == c.location_id])
   c.iso <- loc.table[location_id == c.location_id, ihme_loc_id]
   write.csv(out.pop, paste0(out.dir, '/population_single_age/', c.iso, ".csv"), row.names = F)
+}))
+
+###For India Rural-urban Splitting locations
+india.locs <- loc.table[level>4 & grepl("IND", ihme_loc_id) ,location_id]
+pop <- get_population(age_group_id = c(28, 49:128), location_id = india.locs, year_id = 1970:2019, gbd_round_id = 6, sex_id = 1:2, single_year_age = T)
+dir.create(paste0(out.dir, '/population_single_age/india_splitting_locs/'), showWarnings = F)
+invisible(lapply(inda.locs, function(c.location_id) {
+  out.pop <- copy(pop[location_id == c.location_id])
+  c.iso <- loc.table[location_id == c.location_id, ihme_loc_id]
+  write.csv(out.pop, paste0(out.dir, '/population_single_age/india_splitting_locs/', c.iso, ".csv"), row.names = F)
 }))
 
 pop <- get_population(age_group_id = c(8:20), location_id = c(epp.locs, parent.locs), year_id = 1970:2019, gbd_round_id = 6, sex_id = 1:2)
