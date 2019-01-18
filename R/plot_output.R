ca61c4fc941f69ff5b82fb327a1dde07440ba3a7
djahag <djahag@uw.edu>
2019-01-18 12:32:00 -0800

    take of 2020 and fix facet wrap

--- R/plot_output.R
+++ R/plot_output.R
@@ -140,9 +140,11 @@ plot_15to49 <- function(loc, run.name, compare.run = '180702_numbat_combined', u
 }
 
 plot_age_specific <- function(loc, run.name, compare.run = '180702_numbat_combined'){
+
   data <- fread(paste0('/share/hiv/epp_input/gbd19/', run.name, '/fit_data/', loc, '.csv'))
   data <- data[!agegr == '15-49']
   setnames(data, 'agegr', 'age')
+  data[,age:=sapply(strsplit(age, "-"), "[[", 1)]
   
   ## Comparison run
   compare.dt <- fread(paste0('/snfs1/WORK/04_epi/01_database/02_data/hiv/spectrum/summary/', compare.run, '/locations/', loc, '_spectrum_prep.csv'))
@@ -151,6 +153,7 @@ plot_age_specific <- function(loc, run.name, compare.run = '180702_numbat_combin
   compare.dt <- merge(compare.dt, age.map[,.(age_group_id,age = age_group_name_short)], by = 'age_group_id')
   compare.dt <- compare.dt[,.(age, sex = ifelse(sex_id == 1, 'male', 'female'), type = 'line', year = year_id, 
                               indicator = measure, model = ifelse(compare.run == '180702_numbat_combined', 'GBD2017', compare.run), mean, lower, upper)]
+ 
   
   cur.dt <- fread(paste0('/share/hiv/epp_output/gbd19/', run.name, '/compiled/', loc, '.csv'))
   cur.dt <- get_summary(cur.dt)
@@ -161,24 +164,28 @@ plot_age_specific <- function(loc, run.name, compare.run = '180702_numbat_combin
   both.dt[,model := factor(model)]
   color.list <- c('blue', 'red')
   names(color.list) <- c(run.name, ifelse(compare.run == '180702_numbat_combined', 'GBD2017', compare.run))
+  both.dt <- both.dt[!age %in% c(5,10),]
   ## TODO: age_group_name rather than age?
-  both.dt[,age := factor(age, levels=paste0(seq(5, 80, 5)))]
-  
-  
-  for(c.indicator in c('Incidence', 'Prevalence', 'Deaths')){
+  both.dt[,age := factor(age, levels=paste0(seq(15, 80, 5)))]
+  both.dt <- both.dt[year!=2020,]
+
+   for(c.indicator in c('Incidence', 'Prevalence', 'Deaths')){
     pdf(paste0('/ihme/hiv/epp_output/gbd19/', run.name, '/age_specific_plots/', c.indicator, '/', loc, '.pdf'), width = 10, height = 6)
     for(c.sex in c('male', 'female')){
       plot.dt <- both.dt[sex == c.sex & indicator == c.indicator]
       gg <- ggplot()
+      
       if(nrow(plot.dt[model == 'ANC Site']) > 0){
         gg <- gg + geom_point(data = plot.dt[model == 'ANC Site'], aes(x = year, y = mean, shape = 'ANC Site'), alpha = 0.2)
       }
+      
       gg <- gg + geom_line(data = plot.dt[type == 'line'], aes(x = year, y = mean, color = model)) +
         geom_ribbon(data = plot.dt[type == 'line'], aes(x = year, ymin = lower, ymax = upper,  fill = model), alpha = 0.2) +
         facet_wrap(~age, scales = 'free_y') +
         theme_bw() +
         scale_fill_manual(values=color.list) + scale_colour_manual(values=color.list)  +
         xlab("Year") + ylab("Mean") + ggtitle(paste0(loc.table[ihme_loc_id == loc, plot_name], ' EPPASM Results'))
+      
       if(nrow(plot.dt[model == 'Household Survey']) > 0){
         gg <- gg + geom_point(data = plot.dt[model == 'Household Survey'], aes(x = year, y = mean, shape = 'Household Survey'))
         gg <- gg + geom_errorbar(data = plot.dt[model == 'Household Survey'], aes(x = year, ymin = lower, ymax = upper))
