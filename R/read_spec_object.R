a3bc9b1c4196ba1f6b27fedf9045a5c5528b21fa
djahag <djahag@uw.edu>
2019-08-06 08:49:06 -0700

    fix to geoadjust function call

--- R/read_spec_object.R
+++ R/read_spec_object.R
@@ -66,7 +66,7 @@ read_spec_object <- function(loc, i, start.year = 1970, stop.year = 2019, trans.
       
       print("Merging ANC bias offsets")
       
-      dt <- sub.anc(loc, dt, i, uncertainty=TRUE)
+      dt <- geo_adj(loc, dt, i, uncertainty=TRUE)
     } 
     
     if(sexincrr.sub){
